dad4130232ae07506384980c184bc08ce4091cc8
Daniel Reina <dannyflatiron@gmail.com>
2019-11-11 16:24:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
-def prime?(number)
-  array = []
-  array = (2...(number)).to_a
-  (2...(number)).each do |n|
-    return false if number % n == 0
-  end
-    true
-end
+# def prime?(number)
+#   array = []
+#   array = (2...(number)).to_a
+#   (2...(number)).each do |n|
+#     return false if number % n == 0
+#   end
+#     true
+# end
 
 # def prime?(number)
 #   array = []
@@ -15,3 +15,12 @@ end
 #   end
 #     true
 # end
+
+def prime?(number)
+  array = [4, 6, 8]
+
+  array.each do |n|
+    return false if number % n == 0
+  end
+    true
+end
